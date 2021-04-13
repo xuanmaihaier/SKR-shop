1f5eb82b1208c8a6de9d2a23663a8fdb1d4338c3
xuanmaihaier <1404111969@qq.com>
2021-04-13 20:18:14 +0800

    bug solve

--- skr-shop/src/plugins/antd/index.js
+++ skr-shop/src/plugins/antd/index.js
@@ -2,12 +2,12 @@ import Vue from "vue"
 // 按需引入
 
 import {
-    Button, Radio, Message, Input, Icon, Tabs, Affix, Card
+    Button, Radio, Message, Input, Icon, Tabs, Affix, Card,Row,Col,List,Select,Pagination
 } from "ant-design-vue"
 Vue.prototype.$message = Message
 // use使用
 const vueUse = {
-    Button, Radio, Input, Icon, Tabs, Affix, Card
+    Button, Radio, Input, Icon, Tabs, Affix, Card,Row,Col,List,Select,Pagination
 }
 Object.keys(vueUse).forEach(item => {
     Vue.use(vueUse[item])
