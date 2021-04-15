68a705e9f1ea36b6a3e3f4f1f2887ac233fe3f80
XXX-lsa <303195840@qq.com>
2021-04-15 09:41:54 +0800

    新增加载进度条

---
+++ skr-shop/src/plugins/nprogress/index.js
@@ -0,0 +1,7 @@
+import NProgress from 'nprogress' // 引入nprogress插件
+import 'nprogress/nprogress.css'  // 这个nprogress样式必须引入
+NProgress.configure({
+  showSpinner: false,//关闭NProgress转圈圈
+  easing: 'ease' //进度条速率
+});
+export default NProgress
