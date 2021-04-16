2fd53c0928b44537ccebbbb7b50c08e5386a35a7
xuanmaihaier <1404111969@qq.com>
2021-04-16 15:39:56 +0800

    solve

--- skr-shop/src/network/getList.js
+++ skr-shop/src/network/getList.js
@@ -8,9 +8,9 @@ export async function getTypeOneList(parent_name) {
   return await axios({ method: "post", url: "/type/getproduct", data: { parent_name } })
 }
 /**
- * 获取一级列表
- * url: /type/getproduct
- * 参数：parent_name
+ * 获取二级列表
+ * url: /wares/getSpu
+ * 参数：name
  */
 export async function getTypeOTwoist(name) {
   return await axios({ method: "post", url: "/wares/getSpu", data: { name } })
