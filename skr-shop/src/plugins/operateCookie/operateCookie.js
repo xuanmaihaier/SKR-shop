ba6e3a88fbb489839d235474ad6c3269430a43ce
z2801810010 <2801810010@qq.com>
2021-04-17 15:53:37 +0800

    zjjj

--- skr-shop/src/plugins/operateCookie/operateCookie.js
+++ skr-shop/src/plugins/operateCookie/operateCookie.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 let apis = {
-<<<<<<< HEAD
-
     animateScroll(from, to, callback) {
         let timer = setInterval(function () {
             from = from + (to - from) / 10;
@@ -23,32 +21,6 @@ let apis = {
         }
         document.cookie = str;
     },
-
-
-=======
-  animateScroll(from, to, callback) {
-    let timer = setInterval(function () {
-      from = from + (to - from) / 10;
-      from = to - from > 0 ? Math.ceil(from) : Math.floor(from);
-      if (from == to) {
-        clearInterval(timer);
-        if (typeof callback == "function") callback()
-      }
-      // console.log(from)
-      document.documentElement.scrollTop = from + 1;
-    }, 11.1);
-  },
-  addCookie(objName, objValue, objDays) {
-    let str = objName + "=" + escape(objValue); //编码
-    if (objDays > 0) {//为0时不设定过期时间，浏览器关闭时cookie自动消失
-      let date = new Date();
-      let ms = objDays * 3600 * 1000 * 24;
-      date.setTime(date.getTime() + ms);
-      str += "; expires=" + date.toGMTString();
-    }
-    document.cookie = str;
-  },
->>>>>>> 9e377ca9dfe30d8cc91044a5c80ea89cafd2a6d2
   //读Cookie
   getCookie(objName) {//获取指定名称的cookie的值
     let arrStr = document.cookie.split("; ");
@@ -63,8 +35,6 @@ let apis = {
     document.cookie = objName + "=;expires=" + (new Date(0)).toGMTString();
   }
 }
-
-
 export default {
   install(Vue) {
     Object.keys(apis).forEach((key) => {
