0793cd546dc22df596ce5770e6c1696896eeaeab
XXX-lsa <303195840@qq.com>
2021-04-19 17:57:14 +0800

    17:57

--- skr-shop/src/network/userAddress.js
+++ skr-shop/src/network/userAddress.js
@@ -7,3 +7,11 @@ export async function delAddress(options){
     data:options
   })
 }
+
+export async function addAddress(options){
+  return await axios({
+    method:'post',
+    url:'/user/addAddress',
+    data:options
+  })
+}
